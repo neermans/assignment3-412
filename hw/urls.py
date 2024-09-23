b3c9348c4dc15a0322d961e9eddad715f2f8d050
Raya Neerman <neermans@bu.edu>
2024-09-23 13:55:29 -0400

    adding assignment3 files

---
+++ hw/urls.py
@@ -0,0 +1,13 @@
+## hw/urls.py
+## description: URL patterns for the HW app
+
+from django.urls import path
+from django.conf import settings
+from . import views
+
+
+#all of the URLs that are part of this app
+urlpatterns = [
+    path(r'', views.home, name="home"),
+    path(r'about', views.about, name="about"),
+]
