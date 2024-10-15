a55d39675e1edd202c86d9344178985f9928e29e
Raya Neerman <neermans@bu.edu>
2024-10-14 21:58:30 -0400

    did parts 1 and 2, saving to not f^&(*^ stuff up

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,14 +1,16 @@
 # mini_fb/views.py
-# define the views for the mini_fb app
 
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Profile
 
 class ShowAllProfilesView(ListView):
     model = Profile
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
+
+# New view to show individual profile
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'mini_fb/show_profile.html'
+    context_object_name = 'profile'
