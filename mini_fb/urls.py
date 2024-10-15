1cd301c3f28a381f09f64619af347e7eeae782dc
Raya Neerman <neermans@bu.edu>
2024-10-14 21:58:30 -0400

    did parts 1 and 2, saving to not f^&(*^ stuff up

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,9 +2,9 @@
 # URL patterns for the mini_fb app 
 
 from django.urls import path
-from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path(r'', views.ShowAllProfilesView.as_view(), name='show_all_profiles'),
+    path('', views.ShowAllProfilesView.as_view(), name='show_all_profiles'),
+    path('profile/<int:pk>/', views.ShowProfilePageView.as_view(), name='show_profile'),
 ]
