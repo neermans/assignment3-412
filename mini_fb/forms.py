c88fe8419178d140e242449601e0324d39c21c1a
Raya Neerman <neermans@bu.edu>
2024-10-21 14:57:00 -0400

    assignment6 done

--- mini_fb/forms.py
+++ mini_fb/forms.py
@@ -12,3 +12,8 @@ class CreateStatusMessageForm(forms.ModelForm):
     class Meta:
         model = StatusMessage
         fields = ['message']  # Collect the message
+
+class UpdateProfileForm(forms.ModelForm):
+     class Meta:
+        model = Profile
+        fields = ['city', 'email', 'profile_image_url']  # All fields that should not be changeable
