22d9c90901f303bf20bf4bd0489c3cbbe05e011d
Raya Neerman <neermans@bu.edu>
2024-10-28 09:17:50 -0400

    finished task 1

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -25,6 +25,15 @@ class Profile(models.Model):
     
     def get_status_messages(self):
         return self.statusmessage_set.order_by('-timestamp')  # Retrieve status messages by timestamp
+    
+    def get_friends(self):
+        # get friends depending on whether this profile is profile1 or profile2
+        friends_profile1 = Friend.objects.filter(profile1=self).values_list('profile2', flat=True)
+        friends_profile2 = Friend.objects.filter(profile2=self).values_list('profile1', flat=True)
+
+        # Combine both lists and retrieve Profile objects
+        friend_ids = list(friends_profile1) + list(friends_profile2)
+        return Profile.objects.filter(id__in=friend_ids).distinct()
 
 
 # StatusMessage Model
@@ -51,3 +60,13 @@ class Image(models.Model):
     def __str__(self):
         # Display the idea of an image 
         return f"Image for {self.image} uploaded at {self.uploaded_at}"
+    
+
+# Friend model
+class Friend(models.Model):
+    profile1 = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="friends_profile1")
+    profile2 = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="friends_profile2")
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.profile1} & {self.profile2}"
