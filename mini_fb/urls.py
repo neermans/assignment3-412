e595574d8078af4da06f1c3a6d8e4dd349d98c68
Raya Neerman <neermans@bu.edu>
2024-11-03 12:55:05 -0500

    task 2 done

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -10,13 +10,13 @@ urlpatterns = [
     path('', views.ShowAllProfilesView.as_view(), name='show_all_profiles'), #to show all the profiles
     path('profile/<int:pk>/', views.ShowProfilePageView.as_view(), name='show_profile'), #to show specific profiles
     path('create_profile/', CreateProfileView.as_view(), name='create_profile'), #to create a new profile
-    path('profile/<int:pk>/create_status/', CreateStatusMessageView.as_view(), name='create_status'),  # New URL for status messages
-    path('profile/<int:pk>/update', UpdateProfileView.as_view(), name='update_profile'), # New URL for updating profile
+    path('profile/create_status/', CreateStatusMessageView.as_view(), name='create_status'),  # New URL for status messages
+    path('profile/update', UpdateProfileView.as_view(), name='update_profile'), # New URL for updating profile
     path('profile/status/<int:pk>/delete', views.DeleteStatusMessageView.as_view(), name='delete_status'), # New URL for deleting a status message
     path('profile/status/<int:pk>/update', views.UpdateStatusMessageView.as_view(), name='update_status'), # New URL for updating a status message
-    path('profile/<int:pk>/add_friend/<int:other_pk>', CreateFriendView.as_view(), name='add_friend'), # New URL for creating a friend 
-    path('profile/<int:pk>/friend_suggestions', ShowFriendSuggestionsView.as_view(), name='friend_suggestions'), # New URL for showing friend suggestions for a user
-    path('profile/<int:pk>/news_feed', ShowNewsFeedView.as_view(), name='news_feed'), # New URL for showing the news feed of a profile
+    path('profile/add_friend/<int:other_pk>', CreateFriendView.as_view(), name='add_friend'), # New URL for creating a friend 
+    path('profile/friend_suggestions', ShowFriendSuggestionsView.as_view(), name='friend_suggestions'), # New URL for showing friend suggestions for a user
+    path('profile/news_feed', ShowNewsFeedView.as_view(), name='news_feed'), # New URL for showing the news feed of a profile
     path('login/', auth_views.LoginView.as_view(template_name = 'mini_fb/login.html'), name='login'),  # New URL for login
     path('logout/', auth_views.LogoutView.as_view(template_name='mini_fb/logged_out.html'), name='logout'), # New URL for logout page
     
