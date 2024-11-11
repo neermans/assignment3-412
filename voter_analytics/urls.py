993f390b3ef5509795152836b5e756625f83054d
Raya Neerman <neermans@bu.edu>
2024-11-11 15:57:08 -0500

    task 3 done as well

--- voter_analytics/urls.py
+++ voter_analytics/urls.py
@@ -1,8 +1,10 @@
 # voter_analytics/urls.py
 from django.urls import path
-from .views import VoterListView, VoterDetailView
+from .views import VoterListView, VoterDetailView, GraphsView
 
 urlpatterns = [
     path('', VoterListView.as_view(), name='voters'),
     path('voter/<int:pk>', VoterDetailView.as_view(), name='voter'),  # Detail view for a single voter
+    path('graphs', GraphsView.as_view(), name='graphs'), # view for the graphs
+
 ]
